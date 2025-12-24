27f8b11e798aee66d3f9067febaab0b94a7263cf
Shashankbk4 <shashankk1718@gmail.com>
2025-12-24 15:43:19 +0530

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/com/socialmediamanagement/api/SocialMediaManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.socialmediamanagement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaManagementToolApplication.class, args);
+	}
+
+}
